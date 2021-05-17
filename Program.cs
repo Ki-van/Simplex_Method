5f5d36f983546ce9f71c8ad77bd5312c8c337408
Ki-van <kirill.ivanin00@yandex.ru>
2021-05-17 21:46:42 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace MMK_LB_2_SIMPLEX
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var s = new Simplex(
+              new double[] { 6.5,0,-7.5, 23.5,-5 },
+              new double[,] {
+          {1,3,1,4,0},
+          {2,0,-1,12,-1},
+          {1,2,0,3,-5}
+              },
+              new double[] { 12,14,6 }
+            );
+
+            
+            Console.WriteLine("Максимальное значение целевой функции: ", answer.Item1);
+            Console.WriteLine(string.Join(", ", answer.Item2));
+        }
+    }
+}
---
+++ Simplex.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MMK_LB_2_SIMPLEX
+{
+    class Simplex
+    {
+
+
+        public Simplex(double[] c, double[,] A, double[] b)
+        {
+
+        }
+
+    }
+}
